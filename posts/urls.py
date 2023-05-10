9276a44a2077e4172bf0185069c38e08376af67b
jinyoungpark00 <doghong00@naver.com>
2023-05-11 00:10:14 +0900

    방명록 생성,모두 가져오기 함수 분리 및 함수명 변경

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from posts.views import *
 
 urlpatterns = [
-    path("", post_list, name="post_list"),
-    path("<int:id>", post, name="post"),
+    path("", create_post_list, name="create_post_list"),
+    path("all/", get_post_list_all, name="get_post_list_all"),
+    path("<int:id>", post_list_detail, name="post_list_detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -6,57 +6,63 @@ import json
 
 
 # Create your views here.
-@require_http_methods("[GET], [POST]")
-def post_list(request):
-    if request.method == "GET":
-        posts = Post_list.objects.all()
 
-        posts_json = []
-        for post in posts:
-            post_json = {
-                "post_id": post.post_id,
-                "author": post.author,
-                "title": post.title,
-                "content": post.content,
-            }
 
-            posts_json.append(post_json)
+# 방명록 모두 가져오기
+@require_http_methods("[GET]")
+def get_post_list_all(request):
+    posts = Post_list.objects.all()
+    posts_json = []
 
-        return JsonResponse(
-            {
-                "status": 200,
-                "message": "읽어오기 성공",
-                "data": posts_json,
-            }
-        )
-
-    elif request.method == "POST":
-        body = json.loads(request.body.decode("utf-8"))
-
-        new_post = Post_list.objects.create(
-            author=body["author"],
-            title=body["title"],
-            content=body["content"],
-        )
-
-        new_post_json = {
-            "id": new_post.post_id,
-            "author": new_post.author,
-            "title": new_post.title,
-            "content": new_post.content,
+    for post in posts:
+        post_json = {
+            "post_id": post.post_id,
+            "authoer": post.author,
+            "title": post.title,
+            "content": post.content,
         }
 
-        return JsonResponse(
-            {
-                "status": 200,
-                "message": "post 성공",
-                "data": new_post_json,
-            }
-        )
+        posts_json.append(post_json)
+
+    return JsonResponse(
+        {
+            "status": 200,
+            "massage": "방명록 모두 불러오기 성공",
+            "data": posts_json,
+        }
+    )
 
 
+# 방명록 추가
+@require_http_methods("[POST]")
+def create_post_list(request):
+    body = json.loads(request.body.decode("utf-8"))
+
+    new_post = Post_list.objects.create(
+        author=body["author"],
+        title=body["title"],
+        content=body["content"],
+    )
+
+    new_post_json = {
+        "id": new_post.post_id,
+        "author": new_post.author,
+        "title": new_post.title,
+        "content": new_post.content,
+    }
+
+    return JsonResponse(
+        {
+            "status": 200,
+            "message": "post 성공",
+            "data": new_post_json,
+        }
+    )
+
+
+# 방명록 가져오기, 삭제
 @require_http_methods("[GET], [DELETE]")
-def post(request, id):
+def post_list_detail(request, id):
     if request.method == "GET":
         post = get_object_or_404(Post_list, pk=id)
 
