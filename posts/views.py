a3a681f215638f8dffa81dd70367a7c2a3d3c509
jinyoungpark00 <doghong00@naver.com>
2023-05-12 13:56:34 +0900

    posts/view render import 및 시간대 설정

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+
 from .models import Post_list
 import json
 
