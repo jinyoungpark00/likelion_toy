42486c3024356b603cd523f97a7379505e3fc4ee
jinyoungpark00 <doghong00@naver.com>
2023-05-20 11:32:44 +0900

    CORS port 도메인명 추가

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,9 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:5500",
     "http://127.0.0.1:5500",
+    "http://likelion-toy.kro.kr:5500",
 ]
 
 ROOT_URLCONF = "config.urls"
